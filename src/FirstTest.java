376e73b02579bc7fede71f46082c509213a4dc10
IvanToropygin <itori86@mail.ru>
2022-06-30 22:34:41 +0300

    Ex2: Создание метода

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -14,7 +19,7 @@ public class FirstTest {
     public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "AndroidTestDevice");
+        capabilities.setCapability("deviceName", "and80");
         capabilities.setCapability("platformVersion", "8.0");
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
@@ -25,12 +30,146 @@ public class FirstTest {
     }
 
     @After
-    public void tearDown() {
-        driver.quit();
-    }
+    public void tearDown() {driver.quit();}
 
     @Test
     public void firstTest() {
-        System.out.println("First test run");
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find searchInputOnMainPage",
+                5);
+
+        waitForElementAndSendKeysByXPath(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find searchInputOnSearchPage",
+                5);
+
+        waitForElementPresent(
+                By.xpath("//*[contains(@text, 'Island')]"),
+                "Cannot find searching result",
+                15);
+    }
+
+    @Test
+    public void testCancelSearch(){
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find searchInputOnMainPage",
+                5);
+
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "Cannot find search_close_btn",
+                5);
+
+        waitForElementNotPresentById(
+                By.id("org.wikipedia:id/search_close_btn"),
+                "X btn still present on Page",
+                5);
+    }
+
+    @Test
+    public void testCompareArticleTitle() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find searchInputOnMainPage",
+                5);
+
+        waitForElementAndSendKeysByXPath(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find searchInputOnSearchPage",
+                5);
+
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Island')]"),
+                "Cannot find searchInputOnMainPage",
+                5);
+
+        WebElement titleElement = waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_subtitle_text"),
+                "Cannot find article title",
+                15);
+
+        String titleExpected = "Island in Indonesia, Southeast Asia";
+        String titleActual = titleElement.getAttribute("text");
+
+        Assert.assertEquals(
+                "unexpected title",
+                titleActual,
+                titleExpected
+        );
+    }
+
+    @Test
+    public void testClearSearch(){
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find searchInputOnMainPage",
+                5);
+
+        waitForElementAndSendKeysByXPath(
+                By.xpath("//*[contains(@text, 'Search…')]"),
+                "Java",
+                "Cannot find searchInputOnSearchPage",
+                5);
+
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Field dont cleared",
+                5);
+    }
+
+    @Test
+    public void testSearchTitleTextCompare(){
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/search_container"),
+                "Cannot find searchInputOnMainPage",
+                5);
+        assertElementHasText(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Search Wikipedia",
+                "Search title text don't compare");
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    private WebElement waitForElementPresent(By by, String error_message){
+        return waitForElementPresent(by,error_message, 5);
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSendKeysByXPath(By by, String value, String error_message, long timeoutSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    private boolean waitForElementNotPresentById(By by, String error_message, long timeoutSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+    private WebElement waitForElementAndClear(By by, String error_message, long timeoutSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutSeconds);
+        element.clear();
+        return element;
+    }
+
+    private WebElement assertElementHasText(By by, String value, String error_message){
+        WebElement element = waitForElementPresent(by, error_message);
+        element.getAttribute("text");
+        return element;
     }
 }
